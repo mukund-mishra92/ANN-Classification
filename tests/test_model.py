f0889371e5f048cfecf5936acd46e262e61903e6
mukund-mishra92 <balmukund.mishra92@gmail.com>
2025-07-09 17:39:47 +0530

    adding correct test_model for unit test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,7 @@
-import pytest
-from model.ann import load_model  # example function
+import tensorflow as tf
+#from src import load_model  # example function
 
 def test_model_loading():
-    model = load_model()
+    model = tf.keras.models.load_model('src/customer_churn_model.h5')
+    #model = load_model()
     assert model is not None
